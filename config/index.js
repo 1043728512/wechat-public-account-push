9ac33948c083e708c627ddf7db9f848fdc32af68
1043728512 <114337177+1043728512@users.noreply.github.com>
2022-09-25 20:36:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-/* eslint-disable */
-const USER_CONFIG = {
+/* 禁用 eslint */
+常量 USER_CONFIG  =  {
   /**
    * 公众号配置
    */
@@ -7,110 +7,120 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: '',
+  APP_ID : '' ,
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '',
+  APP_SECRET：'' ，
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
-  IS_SHOW_COLOR: true,
+  IS_SHOW_COLOR：真，
 
   // 功能开关,打开：true，关闭：false
-  SWITCH: {
+  开关：{
     /** 每日天气 */
-    weather: true,
+    天气：真实，
 
     /** 节假日 */
     // 下一休息日综合提醒
-    holidaytts: false,
+    假日：真的，
 
     /** 每日N句 */
     // 金山每日一句
-    CIBA: true,
+    汽巴：真的，
     // 每日一言
-    oneTalk: false,
+    oneTalk：错误，
     // 土味情话(彩虹屁)
-    earthyLoveWords: false,
+    earthyLoveWords：错误，
     // 朋友圈文案
-    momentCopyrighting: false,
+    时刻版权所有：假，
     // 毒鸡汤
-    poisonChickenSoup: false,
+    毒鸡汤：假，
     // 古诗古文
-    poetry: false,
+    诗：假，
 
     /** 星座运势 */
-    horoscope: false,
+    星座运势：假，
 
     /** 生日消息和节日消息 */
-    birthdayMessage: true,
+    生日消息：真实，
 
     // 学生课表
-    courseSchedule: false,
+    课程安排：假，
   },
 
   /** 每日一言 */
 
   // 每日一言的内容类型
   // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-  LITERARY_PREFERENCE: '',
+  LITERARY_PREFERENCE : '' ,
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [yq18787379188
+  用户：[yq18787379188
     {
       // 想要发送的人的名字
-      name: '臭猪0',
+      name: '杨茜0',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o_tom6JXQy0DSvF66gOZn85RdaLc',
+      身份证：'o_tom6JXQy0DSvF66gOZn85RdaLc' ，
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'cUQdPhHyCe0d1FuLhT1lw863GddSIf_rrmN9NetDQJI',
+      useTemplateId : 'cUQdPhHyCe0d1FuLhT1lw863GddSIf_rrmN9NetDQJI' ,
       // 所在省份
       province: '云南',
       // 所在城市
       city: '楚雄',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-22',
+      星座日期：'12-20' ，
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: '',
+      openUrl : 'https://wangxinleo.cn' ,
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: [
+      节日：[
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '生日', name: '杨茜小宝贝', year: '2000', date: '12-27',
+          type: '*生日', name: '老婆', year: '1996', date: '09-09',
         },
+        {
+          type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '杨茜', year: '2000', date: '12-20',
+        },
+        {
+          type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
+        }
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-      customizedDateList: [
+      自定义日期列表：[
         // 在一起的日子
-        { keyword: 'love_day', date: '2018-12-27' },
+        { 关键字：'love_day' ， 日期：'2018-12-27'  } ，
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { 关键字：'marry_day' ， 日期：'2022-09-09'  } ，
         // 退伍日
-        { keyword: 'ex_day', date: '2022-09-10' }
+        { 关键字：'ex_day' ， 日期：'2022-09-10'  }
       ],
       // 课程表相关配置
       // 如果courseSchedule不存在或者为空（null）则认为没有课程
       // 如果courseSchedule是一个数组，则认为不区分单双周，直接填写星期几对应的课表数据即可
       // 如果courseSchedule是一个对象（如下面所示）
-      courseSchedule: {
+      课程安排：{
         // 单双周的基准
-        benchmark: {
+        基准：{
           // 这里设置一个日期，用来作为判断课表是否单双周的依据
-          date: '2022-09-23',
+          日期：'2022-09-23' ，
           // 该日期是否为单周
-          isOdd: true
+          isOdd :真
         },
         // 课表
-        courses: {
+        课程：{
           // 单周课表
           // 从星期一到星期日（星期六和星期日的课表数组可不填写）
-          odd: [
+          奇怪的：[
             // 例子，周一的课表
             [
               '08:00-09:35 高等数学',
@@ -148,7 +158,7 @@ const USER_CONFIG = {
             ]
           ],
           // 双周课表
-          even: [
+          甚至：[
             [],
             [],
             [],
@@ -162,42 +172,42 @@ const USER_CONFIG = {
     },
     {
       name: '老婆1',
-      id: '',
-      useTemplateId: '',
-      province: '',
-      city: '',
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: 'https://wangxinleo.cn',
-      festivals: [],
-      customizedDateList: [],
-      courseSchedule: null
+      身份证：'' ，
+      useTemplateId : '' ,
+      省：'' ，
+      城市：'' ，
+      星座日期：'' ，
+      星座日期类型：'' ，
+      openUrl : 'https://wangxinleo.cn' ,
+      节日：[ ] ，
+      定制日期列表：[ ] ，
+      课程安排：空
     },
     {
       name: '老婆2',
-      id: '',
-      useTemplateId: '',
-      province: '',
-      city: '',
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: 'https://wangxinleo.cn',
-      festivals: [],
-      customizedDateList: [],
-      courseSchedule: null
+      身份证：'' ，
+      useTemplateId : '' ,
+      省：'' ，
+      城市：'' ，
+      星座日期：'' ，
+      星座日期类型：'' ，
+      openUrl : 'https://wangxinleo.cn' ,
+      节日：[ ] ，
+      定制日期列表：[ ] ，
+      课程安排：空
     },
     {
       name: '老婆3',
-      id: '',
-      useTemplateId: '',
-      province: '',
-      city: '',
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: 'https://wangxinleo.cn',
-      festivals: [],
-      customizedDateList: [],
-      courseSchedule: null
+      身份证：'' ，
+      useTemplateId : '' ,
+      省：'' ，
+      城市：'' ，
+      星座日期：'' ，
+      星座日期类型：'' ，
+      openUrl : 'https://wangxinleo.cn' ,
+      节日：[ ] ，
+      定制日期列表：[ ] ，
+      课程安排：空
     }
     // 你可以不断按格式往下增加
     // ...
@@ -208,15 +218,15 @@ const USER_CONFIG = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '8C_xePZYuuEWpVI2SqzdvYE8k7R3x0Vy2QVB-ubA0fc',
+  CALLBACK_TEMPLATE_ID：'' ，
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
-  CALLBACK_USERS: [yyd56377
+  呼叫用户：[
     {
       // 一般都填自己
       name: '自己',
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o_tom6IjhG2MU-ggxHRn4wkepudY',
+      身份证：'' ，
     }
     // 你可以不断按格式往下增加
     // ...
@@ -252,7 +262,7 @@ const USER_CONFIG = {
    * 删除isShowAge属性，也会不展示岁数
    * --- 是否展示周岁信息结束 ---
    */
-  FESTIVALS: [
+  节日：[
     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
     {
       type: '*生日', name: '老婆', year: '1996', date: '09-09', isShowAge: true,
@@ -276,7 +286,7 @@ const USER_CONFIG = {
    * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
    * 如果为0, 则默认展示全部
    */
-  FESTIVALS_LIMIT: 4,
+  FESTIVALS_LIMIT : 4 ,
 
   /** 日期相关 */
 
@@ -284,13 +294,13 @@ const USER_CONFIG = {
    * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
    * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }}
    * */
-  CUSTOMIZED_DATE_LIST: [
+  CUSTOMIZED_DATE_LIST：[
     // 在一起的日子
-    { keyword: 'love_day', date: '2022-09-08' },
+    { 关键字：'love_day' ， 日期：'2022-09-08'  } ，
     // 结婚纪念日
-    { keyword: 'marry_day', date: '2022-09-09' },
+    { 关键字：'marry_day' ， 日期：'2022-09-09'  } ，
     // 退伍日
-    { keyword: 'ex_day', date: '2022-09-10' }
+    { 关键字：'ex_day' ， 日期：'2022-09-10'  }
     // 你可以不断按格式往下增加
     // ...
   ],
@@ -301,13 +311,13 @@ const USER_CONFIG = {
    * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
    * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }}
    * */
-  SLOT_LIST: [
+  SLOT_LIST：[
     // 这样配置的话，就会每次发送这句话
     { keyword: 'encourage_oneself', contents: '你主要的问题在于读书太少而想得太多' },
     // 这样配置的话，就会每次随机选一句话发送
     {
-      keyword: 'lover_prattle',
-      contents: [
+      关键字：'lover_pratle' ，
+      内容：[
         '因为太喜欢你，所以看谁都像是情敌。',
         '申请成为你爱里的永久居民。',
         '你很傻，你很笨，可我还是很羡慕你，因为你有我',
@@ -318,6 +328,7 @@ const USER_CONFIG = {
     // ...
   ],
   // 默认的课表配置
-  COURSE_SCHEDULE: null
+  课程时间表：空
 }
-export default USER_CONFIG
+
+模块。出口 =  USER_CONFIG
